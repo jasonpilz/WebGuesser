917ce57cd4d3d11de9790f7e10c3d588e69b66ad
Jason Pilz <jasonpilz@gmail.com>
2015-10-03 21:34:42 -0600

    I3: Added all guessing logic

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,10 +1,36 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require 'sinatra/cookies'
 require 'pry'
 
 number = rand(100)
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params["guess"].to_i
+
+  message = ""
+  compare = [number, guess]
+  diff = compare.max - compare.min
+
+  if guess != 0
+    if guess > number
+      if diff > 5
+        message = "Way too high!"
+      else
+        message = "Too high!"
+      end
+    elsif guess < number
+      if diff > 5
+        message = "Way too low!"
+      else
+        message = "Too low!"
+      end
+    else
+      message = "You got it right!"
+    end
+  end
+
+  # throw params.inspect
+
+  erb :index, :locals => {:number => number,
+                          :message => message}
 end
